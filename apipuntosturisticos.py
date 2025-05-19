c3782888fba258b085ebe14726fdcc149e36a055
postludecebo <seleon56@soy.sena.edu.co>
2025-05-19 18:52:08 -0500

    Primer commit, API puntos turisticos

---
+++ apipuntosturisticos.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+url = "https://api-colombia.com/api/v1/TouristicAttraction"
+
+try:
+    respuesta = requests.get(url, timeout = 10)
+
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+
+        for atraccion in datos:
+            nombre = atraccion.get("name", "Nombre no disponible")
+            print(f"Nombre: {nombre}")
+
+    
+    else:
+        print(f"Error en la solicitud: Código {respuesta.status_code}")
+    
+except requests.exceptions.RequestException as e:
+    print(f"Error de conexión: {e}")
